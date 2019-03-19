dd0515567b5ac620677e807a879c0cc03e55d3ab
Radonirinaunimi <tanjona.rabemananjara@unimi.it>
2019-03-19 21:54:57 +0100

    Plot the comparison of the loss function

--- PDFs_Replicas/plot_loss.py
+++ PDFs_Replicas/plot_loss.py
@@ -6,22 +6,23 @@ sb.set_style("whitegrid")
 with open('loss.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
+    iteration, DisLoss, GenLoss = [], [], []
     for row in csv_reader:
         if line_count == 0:
             pass
             line_count += 1
         else:
-            iteration = float(row[0])
-            DisLoss   = float(row[1])
-            GenLoss   = float(row[2])
+            iteration.append(float(row[0]))
+            DisLoss.append(float(row[1]))
+            GenLoss.append(float(row[2]))
             line_count += 1
 
 plt.plot(iteration,DisLoss)
 plt.figure()
 dis = plt.plot(iteration,DisLoss)
 gen = plt.plot(iteration,GenLoss)
-# plt.legend((dis,gen), ("Discriminator Loss","Generator Loss"))
-# plt.title('GANs Loss')
+plt.legend([dis[0],gen[0]], ("Discriminator Loss","Generator Loss"))
+plt.title('GANs Loss')
 plt.tight_layout()
 plt.savefig('loss.png', dpi=150)
 plt.close()
