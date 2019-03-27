03c4f2ccb6d5c7cdc79ec7118e7b631734a9ad79
Radonirinaunimi <tanjona.rabemananjara@unimi.it>
2019-03-27 18:27:50 +0100

    Rewrite a few things

--- PDFs_Replicas/GANs_Gluon.py
+++ PDFs_Replicas/GANs_Gluon.py
@@ -1,9 +1,13 @@
 # Import the Libraries
 import lhapdf
+import math
 import tensorflow as tf
 import numpy as np
 import seaborn as sb
+from random import sample
 import matplotlib.pyplot as plt
+from matplotlib.font_manager import FontProperties
+from matplotlib.lines import Line2D
 sb.set_style("whitegrid")
 
 # Get the PDF4LHC15 for test purpose and print some description
@@ -13,16 +17,22 @@ print(pdf.description)
 pdf_central = pdf.mkPDF(0)
 
 # Define the scale 
-Q_pdf = 125
+Q_pdf = 1.5
+
+# Define a log uniform function
+def loguniform(low=0, high=1, size=None):
+    return np.exp(np.random.uniform(low, high, size))
 
 # Define a function which does the sampling
 def sample_pdf(n=1000):
     data  = []
     x_pdf = []
-    for i in np.logspace(-3,-1,n/2):
-        x_pdf.append(i)
-    for i in np.linspace(0.1,1,n/2):
-        x_pdf.append(i)
+    m = math.floor(n/3)
+    # Take m random values between 1e-3 and 1e-1 in logscale
+    for i in loguniform(low=-3,high=-1,size=m): x_pdf.append(i)
+    # Taake (n-m) values between 0.1 and 1 
+    for i in np.random.uniform(0.1,1,n-m): x_pdf.append(i)
+    # Construct the sampling
     for x in x_pdf:
         y_pdf = pdf_central.xfxQ(21,x,Q_pdf)
         data.append([x,y_pdf])
@@ -30,24 +40,34 @@ def sample_pdf(n=1000):
 
 # Define the function which samples the Data
 def sample_noise(m, n):
-    return np.random.uniform(0, 1., size=[m, n])
+    return np.random.uniform(0,1.,size=[m, n])
 
+# Define a function which sort a multi-dimensional list wtr to the 1st row
+def sorting(lst):
+    new_lst = []
+    ordering = lst[0].argsort()
+    for i in range(len(lst)):
+        new_lst.append(lst[i][ordering])
+    return new_lst
+
+# Define the hidden layers 
+hidden = [16,16]
 # Implement the GENERATOR Model
 
 # The following function takes the follwoing as input:
 # A Random Sample Z
 # A list which contains the Structure of the NN (layers)
 # A variable called "reuse"
-def generator(Z, layers_size=[16, 16],reuse=False):
+def generator(input_noise, layers_size=hidden,reuse=False):
     # Create and Share a variable named "Generator"
     # Here "reuse" allows us to share the variable
     with tf.variable_scope("Generator",reuse=reuse):
         # Define the 1st and 2nd layer with "leaky_relu" as an activation fucnction
-        l1 = tf.layers.dense(Z,layers_size[0],activation=tf.nn.leaky_relu)
-        l2 = tf.layers.dense(l1,layers_size[1],activation=tf.nn.leaky_relu)
+        L1 = tf.layers.dense(input_noise,layers_size[0],activation=tf.nn.leaky_relu)
+        L2 = tf.layers.dense(L1,layers_size[1],activation=tf.nn.leaky_relu)
         # Define the output layer with 2 nodes
         # This dimension is correspond to the dimension of the "real dataset"
-        output = tf.layers.dense(l2,2)
+        output = tf.layers.dense(L2,2)
 
     return output
 
@@ -57,18 +77,18 @@ def generator(Z, layers_size=[16, 16],reuse=False):
 # A sample from the "REAL" dataset
 # A list which contains the structure of the first 2 "hidden layer"
 # A variable called "reuse"
-def discriminator(X,layers_size=[16, 16],reuse=False):
+def discriminator(input_true,layers_size=hidden,reuse=False):
     # Create and share a variable named "Discriminator"
     with tf.variable_scope("Discriminator",reuse=reuse):
         # Define the 1st and 2nd layer with "leaky_relu" as an activation fucnction
-        l1 = tf.layers.dense(X,layers_size[0],activation=tf.nn.leaky_relu)
-        l2 = tf.layers.dense(l1,layers_size[1],activation=tf.nn.leaky_relu)
+        L1 = tf.layers.dense(input_true,layers_size[0],activation=tf.nn.leaky_relu)
+        L2 = tf.layers.dense(L1,layers_size[1],activation=tf.nn.leaky_relu)
         # Fix the third layer to 2 nodes so we can visualize the transformed feature space in a 2D plane
-        l3 = tf.layers.dense(l2,2)
+        L3 = tf.layers.dense(L2,2)
         # Define the output layer (logit)
-        output = tf.layers.dense(l3,1)
+        output = tf.layers.dense(L3,1)
 
-    return output, l3
+    return output, L3
 
 # Adversarial Training
 
@@ -87,11 +107,11 @@ DiscriminatorLoss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logit
                            tf.nn.sigmoid_cross_entropy_with_logits(logits=f_logits,labels=tf.zeros_like(f_logits)))
 GeneratorLoss     = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=f_logits,labels=tf.ones_like(f_logits)))
 
-# Train the NN based on the Model defined above
+# Collect the variables in the graph
 GeneratorVars     = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES,scope="Generator")
 DiscriminatorVars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES,scope="Discriminator")
 
-# Define the Miniminzer for G&D
+# Define the Optimizer for G&D
 GeneratorStep     = tf.train.RMSPropOptimizer(learning_rate=0.001).minimize(GeneratorLoss,var_list = GeneratorVars)
 DiscriminatorStep = tf.train.RMSPropOptimizer(learning_rate=0.001).minimize(DiscriminatorLoss,var_list = DiscriminatorVars)
 
@@ -103,6 +123,7 @@ batch_size = 256
 nd_steps   = 10
 ng_steps   = 10
 
+# Fetch the real PDF in order to plot them
 x_plot = sample_pdf(n=batch_size)
 
 f = open('loss.csv','w')
@@ -110,33 +131,36 @@ f.write('Iteration,Discriminator Loss,Generator Loss\n')
 
 numb_training = 10001
 
+# Training
 for i in range(numb_training):
     X_batch = sample_pdf(n=batch_size)
     Z_batch = sample_noise(batch_size, 2)
 
+    # Train independently G&D in multiple steps
     for _ in range(nd_steps):
         _, dloss = sess.run([DiscriminatorStep, DiscriminatorLoss], feed_dict={X: X_batch, Z: Z_batch})
-    rrep_dstep, grep_dstep = sess.run([r_rep, g_rep], feed_dict={X: X_batch, Z: Z_batch})
 
     for _ in range(ng_steps):
         _, gloss = sess.run([GeneratorStep, GeneratorLoss], feed_dict={Z: Z_batch})
-
-    rrep_gstep, grep_gstep = sess.run([r_rep, g_rep], feed_dict={X: X_batch, Z: Z_batch})
 
     print ("Iterations: %d\t out of %d\t. Discriminator loss: %.4f\t Generator loss: %.4f"%(i,numb_training-1,dloss,gloss))
     if i%100 == 0:
         f.write("%d,%f,%f\n"%(i,dloss,gloss))
 
+    # Plot each 1000 iteration
     if i%1000 == 0:
-        plt.figure()
+        
+        # Fetch the generated data
         g_plot = sess.run(G_sample, feed_dict={Z: Z_batch})
-        xax = plt.scatter(x_plot[:,0], x_plot[:,1])
+
+        plt.figure()
+        xax = plt.scatter(x_plot[:,0],x_plot[:,1])
         gax = plt.scatter(g_plot[:,0],g_plot[:,1])
 
-        plt.legend((xax,gax), ("Real Data","Generated Data"))
+        plt.legend((xax,gax), ("Real PDF","Generated PDF"))
         plt.title('Samples at Iteration %d'%i)
         plt.tight_layout()
-        plt.savefig('plots/iterations/iteration_%d.png'%i, dpi=250)
+        plt.savefig('iterations/iteration_%d.png'%i, dpi=250)
         plt.close()
 
 f.close()
--- PDFs_Replicas/animation.py
+++ PDFs_Replicas/animation.py
@@ -1,10 +1,13 @@
 from PIL import Image
 import numpy as np
 import imageio
+import os
 
 ipath = "iterations/iteration_%d.png"
 
 images = []
-for i in range(11):
+nb_images = len(next(os.walk('iterations'))[2])
+
+for i in range(nb_images):
     images.append(imageio.imread(ipath%(i*1000)))
-imageio.mimsave('iterations.gif', images, fps=1)
+imageio.mimsave('animation/animation.gif', images, fps=1)
